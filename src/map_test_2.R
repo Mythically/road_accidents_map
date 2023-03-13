9891d1e55f8e4803b463ef54d8db1188eb588061
Mythically <60433250+Mythically@users.noreply.github.com>
2023-03-13 11:50:06 +0000

    get color no working, the rest is.

--- src/map_test_2.R
+++ src/map_test_2.R
@@ -1,59 +1,72 @@
 library(leaflet)
 library(dplyr)
 library(shiny)
-
+library(leaflet.extras)
 accidents_data <- read.csv("resources/dft-road-casualty-statistics-accident-2021.csv")
-accidents_data <- accidents_data %>% filter(accident_severity == 3 | accident_severity == 2)
-accidents_data <- accidents_data %>% filter(!is.na(latitude) & !is.na(longitude), accident_severity == 3 | accident_severity == 2)
+accidents_data <- accidents_data %>% filter(accident_severity == 1 | accident_severity == 2)
+accidents_data <- accidents_data %>% filter(!is.na(latitude) &
+                                              !is.na(longitude) &
+                                              (accident_severity == 1 | accident_severity == 2))
+accidents_data <- na.omit(accidents_data[c("longitude", "latitude", "date", "accident_severity", "time", "number_of_vehicles")])
 accidents_data$longitude <- as.numeric(accidents_data$longitude)
 accidents_data$latitude <- as.numeric(accidents_data$latitude)
-accidents_data <- na.omit(accidents_data[c("longitude", "latitude")])
+accidents_data <- na.omit(accidents_data)
 
 ui <- fluidPage(
-  leafletOutput("map"),
+  leafletOutput("map", height = "100vh", width = "100vw")
 )
 
 server <- function(input, output, session) {
+  data <- reactive({
+    x <- accidents_data
+  })
+
   output$map <- renderLeaflet({
-    leaflet() %>%
-      addTiles() %>%
-      setView(lng = 2.2426, lat = 53.4808, zoom = 10)
-  })
+    a_d <- data()
 
-  observe( priority = 1,{
-    req(input$map_zoom)
-    print(input$map_zoom)
-    if (input$map_zoom >= 15) {
-      bounds <- input$map_bounds
-      print(bounds)
-      accidents_data_filtered <- accidents_data %>%
-        filter(longitude >= bounds$west,
-               longitude <= bounds$east,
-               latitude >= bounds$south,
-               latitude <= bounds$north)
-      accidents_data_filtered <- accidents_data_filtered %>% slice(1:10) # Select the first 10 rows
-      print(nrow(accidents_data_filtered))
-      leafletProxy("map", session) %>%
-        addMarkers(data = accidents_data_filtered,
-                   lng = accidents_data_filtered$longitude,
-                   lat = accidents_data_filtered$latitude,
-                   popup = paste(
-                     "Accident Severity: ", accidents_data_filtered$accident_severity,
-                     "Accident Date: ", accidents_data_filtered$date,
-                     "Accident Time: ", accidents_data_filtered$time,
-                     "Number of Vehicles: ", accidents_data_filtered$number_of_vehicles,
-                     sep = "<br/>"
-                   ),
-                   clusterOptions = markerClusterOptions(
-                     maxClusterRadius = 5,
-                     disableClusteringAtZoom = 80
-                   )
-        )
-    } else {
-      leafletProxy("map", session) %>% clearMarkers()
+    # http://rstudio.github.io/leaflet/markers.html
+    getColor <- function(accident) {
+      sapply(accident$accident_severity, function(severity) {
+        if (severity == 1) {
+          return("red")
+        } else if (severity == 2) {
+          return("orange")
+        }
+      })
     }
-  })
 
+    icons <- awesomeIcons(
+      icon = "ios-close",
+      iconColor = "black",
+      library = "ion",
+      markerColor = getColor(a_d)
+    )
+    m <- leaflet() %>%
+      addTiles() %>%
+      addAwesomeMarkers(data = a_d,
+                 lng = a_d$longitude, lat = a_d$latitude,
+                 icon = icons(),
+                 popup = paste(
+                   "Accident Severity: ", a_d$accident_severity,
+                   "Accident Date: ", a_d$date,
+                   "Accident Time: ", a_d$time,
+                   "Number of Vehicles: ", a_d$number_of_vehicles,
+                   sep = "<br/>"
+                 ),
+                 clusterOptions = markerClusterOptions(
+                   maxClusterRadius = 80,
+                   disableClusteringAtZoom = 15),
+
+      ) %>%
+      addSearchOSM(
+        options = searchOptions(
+          url = "https://nominatim.openstreetmap.org/search",
+          zoom = 16,
+          autoCollapse = TRUE,
+          hideMarkerOnCollapse = TRUE
+        )
+      )
+  })
 }
 
 shinyApp(ui, server)
